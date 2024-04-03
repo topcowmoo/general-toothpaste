5c9c33fd751bf93338ee23ec7d0b42a27105237c
Salvatore <mammoliti.sam@gmail.com>
2024-04-03 14:27:19 -0400

    Defined functions that handle the logic of interacting with the IndexedDB database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ content });
+  const result = await request.catch(error => {
+    console.error('Error while saving data to the database:', error);
+    throw error;
+  });
+  console.log('Data saved to the database', result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request.catch(error => {
+    console.error('Error while retrieving data from the database:', error);
+    throw error;
+  });
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
